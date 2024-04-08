7ea37b2301cfa20a96eedbf47f23c7d2205a4827
miciny <15201114041@163.com>
2024-04-09 01:18:01 +0800

    youhua 21

--- deal_smoke/smoke_script.py
+++ deal_smoke/smoke_script.py
@@ -40,32 +40,56 @@ def single_run(smoke_id, pay_type=1):
             raise Exception("没找到现金选择按钮")
         click_screen(cash_page, delay_sec=1)
 
-        # 现金确认
+        # 收款确认
         cash_confirm_page = get_pic_position("cash_confirm", 'deal_smoke/pic', pic_region)
         if not cash_confirm_page:
             raise Exception("没找到现金确认")
         click_screen(cash_confirm_page, delay_sec=1)
         time.sleep(1)
+
+        # 检查输入框，是不是在首页
+        input_page = get_pic_position("input_1", 'deal_smoke/pic', pic_region)
+        if not input_page:
+            raise Exception("现金收款完成，但不在首页")
     
     # 微信
     else:
-        # 选择现金
-        cash_page = get_pic_position("cash", 'deal_smoke/pic', pic_region)
-        if not cash_page:
-            raise Exception("没找到现金选择按钮")
-        click_screen(cash_page, delay_sec=1)
+        pay_no = ""
+        for i in range(300):
+            res = get_pay_no()
+            if res and res['code'] == 0:
+                pay_no = res['data']
+                if pay_no and str(pay_no) == 18:
+                    break
+            time.sleep(1)
 
-        # 现金确认
+        if not pay_no:
+            raise Exception("没找到微信收款码")
+
+        auto_input(pay_no)
+        time.sleep(1)
+
+        # 收款确认
         cash_confirm_page = get_pic_position("cash_confirm", 'deal_smoke/pic', pic_region)
         if not cash_confirm_page:
             raise Exception("没找到现金确认")
         click_screen(cash_confirm_page, delay_sec=1)
-        time.sleep(1)
+        time.sleep(5)
 
-    # 检查输入框，是不是在首页
-    input_page = get_pic_position("input_1", 'deal_smoke/pic', pic_region)
-    if not input_page:
-        raise Exception("收款完成，但不在首页")
+        not_found = 0
+        for i in range(30):
+            pay_check_page = get_pic_position("pay_check", 'deal_smoke/pic', pic_region)
+            if pay_check_page:
+                click_screen(pay_check_page, delay_sec=1)
+                not_found = 0
+            else:
+                # 检查输入框，是不是在首页
+                input_page = get_pic_position("input_1", 'deal_smoke/pic', pic_region)
+                if not input_page:
+                    not_found += 1
+                if not_found > 2:
+                    raise Exception("微信收款可能出现问题")
+            time.sleep(5)
 
 
 def get_pay_info():
@@ -144,3 +168,8 @@ def get_pay_no():
     if res.status_code == 200:
         return res.json()
     return None
+
+
+if __name__ == '__main__':
+    ret = get_this_time_info()
+    print(ret)
